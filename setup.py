778e119fbefd6434cb4e235f0f7254ceaf7a4795
Elsie Powell <elsie+git@elsiepowell.com>
2016-11-05 17:53:54 -0400

    Release 0.1.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open('dev-requirements.txt') as dev_requirements_file:
 
 setup(
     name='sopel_modules.twitter',
-    version='0.1.0',
+    version='0.1.1',
     description='A Twitter module for Sopel',
     long_description=readme + '\n\n' + history,
     author='Elsie Powell',
