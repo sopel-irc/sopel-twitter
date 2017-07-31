e5801c27fad31ad91e943bf47ba78f24c0ae6c1f
Sujeet Akula <sujeet@freeboson.org>
2017-07-31 16:32:41 +1000

    update

--- sopel_modules/twitter/twitter.py
+++ sopel_modules/twitter/twitter.py
@@ -36,36 +36,34 @@ def setup(bot):
     bot.config.define_section('twitter', TwitterSection)
 
 
-@module.url('https?://twitter.com/([^/]*)(?:/status/(\d+)).*')
+@module.url('https?://twitter.com/(?:[^/]*)(?:/status(?:es)?/(\d+)).*')
 def get_url(bot, trigger, match):
-    consumer_key = bot.config.twitter.consumer_key
-    consumer_secret = bot.config.twitter.consumer_secret
+    auth = tweepy.OAuthHandler(bot.config.twitter.consumer_key,
+                               bot.config.twitter.consumer_secret)
+    auth.set_access_token(bot.config.twitter.access_token,
+                          bot.config.twitter.access_token_secret)
+    api = tweepy.API(auth_handler=auth, wait_on_rate_limit=True)
 
-    consumer = oauth.Consumer(key=consumer_key, secret=consumer_secret)
-    client = oauth.Client(consumer)
-    id_ = match.group(2)
-    response, content = client.request(
-        'https://api.twitter.com/1.1/statuses/show/{}.json'.format(id_))
-    if response['status'] != '200':
-        logger.error('%s error reaching the twitter API for %s',
-                     response['status'], match.group(0))
+    id_ = match.group(1)
+    tweet = api.get_status(id_)
 
-    content = json.loads(content.decode('utf-8'))
-    message = ('[Twitter] {content[text]} | {content[user][name]} '
-               '(@{content[user][screen_name]}) | {content[retweet_count]} RTs '
-               '| {content[favorite_count]} ♥s').format(content=content)
-    all_urls = content['entities']['urls']
-    if content['is_quote_status']:
-        message += ('| Quoting {content[quoted_status][user][name]} '
-                    '(@{content[quoted_status][user][screen_name]}): '
-                    '{content[quoted_status][text]}').format(content=content)
-        quote_id = content['quoted_status']['id_str']
-        for url in content['entities']['urls']:
+    message = ('[Twitter] {tweet.text} | {tweet.user.name} '
+               '(@{tweet.user.screen_name}) | {tweet.retweet_count} RTs '
+               '| {tweet.favorite_count} ♥s').format(tweet=tweet)
+    all_urls = tweet.entities['urls']
+    if tweet.is_quote_status:
+        # add the quoted tweet
+        message += (' | Quoting {tweet.quoted_status["user"]["name"]} '
+                    '(@{tweet.quoted_status["user"]["screen_name"]}): '
+                    '{tweet.quoted_status["text"]}').format(tweet=tweet)
+        quote_id = tweet.quoted_status['id_str']
+        # remove the link to the quoted tweet
+        for url in tweet.entities.urls:
             expanded_url = url['expanded_url']
             if expanded_url.rsplit('/', 1)[1] == quote_id:
                 message = message.replace(url['url'], '')
                 break
-        all_urls = all_urls + content['quoted_status']['entities']['urls']
+        all_urls = all_urls + tweet.quoted_status['entities']['urls']
     all_urls = ((u['url'], u['expanded_url']) for u in all_urls)
     all_urls = sorted(all_urls, key=lambda pair: len(pair[1]))
 
@@ -79,7 +77,7 @@ def get_url(bot, trigger, match):
     bot.say(message)
 
 # avoid status urls
-@module.url('(?=.*https?://twitter.com/(?:#!/)?[A-Za-z0-9_]{1,15}(?!/status))https?://twitter.com/(?:#!/)?([A-Za-z0-9_]{1,15}).*')
+@module.url(r'https?://twitter.com/(?:#!/)?([A-Za-z0-9_]{1,15})(?!/status)\b')
 def get_url(bot, trigger, match):
     auth = tweepy.OAuthHandler(bot.config.twitter.consumer_key,
                                bot.config.twitter.consumer_secret)
